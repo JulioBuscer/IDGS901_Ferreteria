d71649d7c42a6d1e77f8a89358f97ebf02bd75e3
DeadCousing <57259646+DeadCousing@users.noreply.github.com>
2021-08-04 18:01:36 -0500

    modificacion de rutas en el middleware

--- IDGS_ferreteria/routes/web.php
+++ IDGS_ferreteria/routes/web.php
@@ -24,10 +24,11 @@ Route::group(['middleware' => ['auth']], function () {
 
     // Agregamos los recursos de compras
     Route::resource('compras', 'ComprasController');
+    Route::resource('/Productos', 'ProductosController');
+    Route::resource('/Categorias', 'CategoriaController');
+    Route::resource('/Catalogo', 'CatalogoProductos');
 });
-Route::resource('/Productos', 'ProductosController');
-Route::resource('/Categorias', 'CategoriaController');
-Route::resource('/Catalogo', 'CatalogoProductos');
+
 Route::put('/Categorias', 'CategoriasController@update');
 Route::get('/login', 'SiteController@login')->name('login');
 Route::post('/login', 'SiteController@loginPost');
